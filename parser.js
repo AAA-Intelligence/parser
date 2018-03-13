bfdf36df303b5e4c5bd19bb52b781381f8d1e516
Leon Erath <leon-erath@hotmail.de>
2018-03-13 21:31:51 +0100

    improved code & fixed bugs

--- parser.js
+++ parser.js
@@ -1,102 +1,104 @@
 var fs = require('fs');
 
-var user1 = "Leon Erath"
-var user2 = "Daniel Salomon"
+var USER_1 = "Leon Erath"
+var USER_2 = "Daniel Salomon"
 var DATE_START  = 0
 var DATE_END    = 15
 var USER_START  = 17
 
 
 
-function readLines(input, func) {
+function parseText(input) {
     var remaining = '';
-    var test = ""
+    
   
     input.on('data', function(data) {
         remaining += data;
-        var index = remaining.indexOf('\n');
-        var last  = 0;
-
-        // get rid of first line
-        var line1 = remaining.substring(last, index);
-        last = index + 1;
-        index = remaining.indexOf('\n', last);
-        
-        while (index > -1) {
-            // get rid of \n from one text
-
-            var line = remaining.substring(last, index);
-            last = index + 1;
-            index = remaining.indexOf('\n', last);
-            var line2 = remaining.substring(last, index);
-            
-            while(parseDate(line2) == false){
-                line = line+" "+line2
-                last = index + 1;
-                index = remaining.indexOf('\n', last);
-                line2 = remaining.substring(last, index);
-            }
-            
-            // group texts from the same author within 2 hours
-            
-
-            line = line.replace(user1,"A")
-            line = line.replace(user2,"B")
-            line2 = line2.replace(user1,"A")
-            line2 = line2.replace(user2,"B")
-
-            var boolean = true
-            while(boolean){    
-                if(line.charAt(18)== line2.charAt(18)){
-                  
-                    var date1 = parseDate(line)
-                    var date2 = parseDate(line2)
-                    let timeDifference = Math.abs(date2.getTime() - date1.getTime());
-                    let differentHours = Math.ceil(timeDifference / (1000 * 3600));
-                    
-                    if(differentHours <= 2){
-                        line2 = line2.substring(20,line2.length)
-                        if(line.charAt(line.length-1)!= "."&&line.charAt(line.length-1)!= "?"&&line.charAt(line.length-1)!= "!"){
-                            line = line +"."
-                        }
-                        line = line + line2
-                        
-                        
-                        last = index + 1;
-                        index = remaining.indexOf('\n', last);
-                        line2 = remaining.substring(last, index);
-                        line2 = line2.replace(user1,"A")
-                        line2 = line2.replace(user2,"B")
-
-                        
-                        
-                    }else{
-                        boolean = false
-                    }
-                }else{
-                    boolean = false
-                }
-            }
-            
-            test += func(line) 
-        }
-      
-  
-      remaining = remaining.substring(last);
+       
     });
   
     
     input.on('end', function() {
-      if (remaining.length > 0) {
-        test += func(remaining)
-       
-      }
-      createFile(test)
-    });
-  
-    
+        paragraphTexts(remaining)
+    });  
+
 }
 
+function paragraphTexts(data){
+    
+    var text = ""
+    
+    var lines = data.split('\n');
+    for(var i = 1;i < lines.length-1;i++){
+        // get rid of \n from one text
+        
+        var line = lines[i]
+        var line2 = lines[i+1]
+       
+        
+        k=0
+        while(parseDate(line2) == false && i+k+1 < lines.length-1){
+            k++
+            line = line+" "+line2
+            line2 = lines[i+1+k]
+        }
+        line = line +"\n"
+        if(line.indexOf("http") > -1 || line.indexOf("<Medien weggelassen>") > -1 ) {
+            line = ""
+        }
+        line = line.replace(USER_1,"A")
+        line = line.replace(USER_2,"B")
+
+        i = i+k
+        text += line 
+
+    }
+    groupTexts(text)
+}
+
+function groupTexts(data){
+  
+    var groupedText = ""
+
+    var lines = data.split('\n');
+    for(var i = 0;i < lines.length-1;i++){
+   
+        var line = lines[i]
+        var line2 = lines[i+1]
+       
+       
+        var date1 = parseDate(line)
+        var date2 = parseDate(line2)
+       
+        let timeDifference = Math.abs(date2.getTime() - date1.getTime());
+        let differentHours = Math.ceil(timeDifference / (1000 * 3600));
+            
+        if(differentHours > 2){
+            line = line + "\n"   
+            boolean = false
+        }
+       
+
+        k= 0;
+        while(line.charAt(18)== line2.charAt(18)){  
+                        k++
+                        line2 = line2.substring(20,line2.length)
+                        
+                        if(line.charAt(line.length-1)!= "."&&line.charAt(line.length-1)!= "?"&&line.charAt(line.length-1)!= "!"){
+                            line = line +"."
+                        }
+
+                        line = line + line2
+                        line2 = lines[i+1+k]
+            
+        }
+        i = i +k
+        groupedText = groupedText+line+"\n";
+        console.log(line);   
+    }
+    createFile(groupedText)
+  
+}
 
 function createFile(data){
     fs.writeFile("export/chat.txt", data, function(err) {
@@ -110,7 +112,8 @@ function createFile(data){
 
 
 function parseDate(input) {
-    input = input.substring(0,15)
+    input = input.substring(DATE_START,DATE_END)
+    
     input = input.replace(",",".")
     input = input.replace(":",".")
     input = input.replace(" ","")
@@ -128,20 +131,11 @@ Date.prototype.addHours= function(h){
     return this;
 }
 
-function func(data) {
 
-    data = data.substring(17,data.length)
-    data = data.replace(user1,"A")
-    data = data.replace(user2,"B")
+// reads chat.txt into input
+var input = fs.createReadStream('chat_full.txt');
 
-    if(data.indexOf("http") > -1 || data.indexOf("<Medien weggelassen>") > -1 ) {
-        data = ""
-    }else{
-        data = data +"\n"
-    }
-    
-    return data
-  }
+// parses Text with given input
+// func is the given parsing function
+parseText(input);
 
-var input = fs.createReadStream('chat.txt');
-readLines(input, func);
