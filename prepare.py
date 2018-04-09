044623398fc5a66a986e52cbd8e39a0e268c946c
Niklas Korz <korz.niklask@gmail.com>
2018-04-09 15:22:24 +0200

    Randomize evaluation data picks

--- prepare.py
+++ prepare.py
@@ -1,4 +1,5 @@
 import math
+import random
 from os import path
 import nltk
 import emoji
@@ -30,21 +31,27 @@ def write_data(data, name):
         f.write('\n'.join(data))
 
 
-src = []
-tgt = []
+src_train = []
+tgt_train = []
 
 with open('export/chat.txt', 'r', encoding='utf-8') as f:
     for i, line in enumerate(f):
         if i % 2 == 0:
-            src.append(normalize(line))
+            src_train.append(normalize(line))
         else:
-            tgt.append(normalize(line))
+            tgt_train.append(normalize(line))
 
-i = math.floor(len(src) * 0.8)
-src_train = src[:i]
-src_val = src[i:]
-tgt_train = tgt[:i]
-tgt_val = tgt[i:]
+i_max = len(src_train)
+src_val = []
+tgt_val = []
+
+for _ in range(math.floor(i_max * 0.2)):
+    i = random.randint(0, i_max)
+    src_val.append(src_train[i])
+    del src_train[i]
+    tgt_val.append(tgt_train[i])
+    del tgt_train[i]
+    i_max -= 1
 
 write_data(src_train, 'src-train')
 write_data(src_val, 'src-val')
