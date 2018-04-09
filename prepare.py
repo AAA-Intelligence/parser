5d80fbd0f2c08577b9cee2c1ea3507f3818b6125
Niklas Korz <korz.niklask@gmail.com>
2018-04-09 15:09:28 +0200

    Handle emojis in prepare script, add whitespace in front

--- prepare.py
+++ prepare.py
@@ -1,17 +1,31 @@
 import math
 from os import path
 import nltk
-
-nltk.download('punkt', quiet=True)
+import emoji
 
 print('Normalizing and splitting data...')
 
+color_emojis = [
+    '🏻',
+    '🏼',
+    '🏽',
+    '🏾',
+    '🏿'
+]
+
+
+def normalize_emojis(text):
+    return ''.join([c if c not in emoji.UNICODE_EMOJI or c in color_emojis else ' ' + c for c in text]).replace('  ', ' ')
+
+
 def normalize(text):
-    return ' '.join(nltk.word_tokenize(text, language='german'))
+    return ' '.join(nltk.word_tokenize(normalize_emojis(text), language='german'))
+
 
 def write_data(data, name):
     with open(path.join('export', name + '.txt'), 'w', encoding='utf-8') as f:
         f.write('\n'.join(data))
+
 
 src = []
 tgt = []
