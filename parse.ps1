c43e68805f0a5c0a37250c54562a9eb4eb2a2f2b
Niklas Korz <korz.niklask@gmail.com>
2018-04-14 11:07:49 +0200

    Update Powershell script

--- parse.ps1
+++ parse.ps1
@@ -1,5 +1,5 @@
-mkdir -p chat-data
-node .\parser.js $args[0] $args[1] $args[2]
+New-Item -ItemType Directory -Force -Path chat-data
+node .\parser.js $args[0] $args[1]
 python .\prepare.py
 onmt-build-vocab --size 50000 --save_vocab chat-data\src-vocab.txt chat-data\src-train.txt
 onmt-build-vocab --size 50000 --save_vocab chat-data\tgt-vocab.txt chat-data\tgt-train.txt
